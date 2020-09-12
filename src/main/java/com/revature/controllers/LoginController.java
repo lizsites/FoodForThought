e951350629a73089b1b40b6740291e09fce30603
lizsites <69205363+lizsites@users.noreply.github.com>
2020-09-11 19:20:11 -0500

    messing with branch

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -1,5 +1,40 @@
 package com.revature.controllers;
 
-public class LoginController {
+import java.io.BufferedReader;
+import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.models.User;
+
+public class LoginController {
+	
+	
+	public void login(HttpServletRequest req, HttpServletResponse res) throws IOException{
+		
+		HttpSession sess = req.getSession();
+		BufferedReader reader = req.getReader();
+
+		StringBuilder sb = new StringBuilder();
+
+		String line = reader.readLine();
+
+		while (line != null) {
+			sb.append(line);
+			line = reader.readLine();
+		}
+
+		String body = new String(sb);
+		ObjectMapper om = new ObjectMapper();
+		User u = om.readValue(body, User.class);
+		
+		if (ls.login(u)) {
+			
+		}
+		
+		
+	}
 }

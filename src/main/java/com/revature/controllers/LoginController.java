f9de3735d90024bae224bb070e615f9dbb7ed7f6
lizsites <69205363+lizsites@users.noreply.github.com>
2020-09-13 15:23:06 -0500

    setting up for add-user

--- src/main/java/com/revature/controllers/LoginController.java
+++ src/main/java/com/revature/controllers/LoginController.java
@@ -35,15 +35,19 @@ public class LoginController {
 
 		String body = new String(sb);
 		User u = om.readValue(body, User.class);
-
+		System.out.println("login controllers u" + u);
 		if (ls.login(u)) {
+			
 			UserDAO userDAO = new UserDAOImp();
 			HttpSession sesh = req.getSession();
 			User f = userDAO.getUserByUsername(u.getUsername());
 			u = f;
 			sesh.setAttribute("user", u);
 			sesh.setAttribute("loggedin" , true);
-			res.getWriter().println(u);
+			String jsonU = om.writeValueAsString(u);
+			
+			res.getWriter().println(jsonU);
+			
 			res.setStatus(200);
 			
 	}

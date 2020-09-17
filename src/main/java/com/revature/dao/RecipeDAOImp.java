7d9fa75c0bfab62c959607d63b50dc1b04669a87
Nicole Lang <nicole.lang@revature.net>
2020-09-16 22:18:50 -0400

    Spring ORM

--- src/main/java/com/revature/dao/RecipeDAOImp.java
+++ src/main/java/com/revature/dao/RecipeDAOImp.java
@@ -1,108 +1,81 @@
 package com.revature.dao;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.models.Recipe;
-import com.revature.utilities.HibernateUtil;
 
+@Repository
+@Transactional
 public class RecipeDAOImp implements RecipeDAO {
 
+	private SessionFactory sf;
+	
+	@Autowired
+	public RecipeDAOImp(SessionFactory sf) {
+		super();
+		this.sf = sf;
+	}
+	
 	@Override
 	public boolean addRecipe(Recipe r) {
-		Session sess = HibernateUtil.getSession();
-		Transaction tx;
-
-		tx = sess.beginTransaction();
-		try {
+		Session sess = sf.getCurrentSession();
+		if(r!=null) {
 			sess.save(r);
-			tx.commit();
 			return true;
-		} catch (HibernateException e) {
-			e.printStackTrace();
-			tx.rollback();
+		}else
 			return false;
-		} finally {
-			sess.close();
-		}
+			
 	}
 
 	@Override
 	public boolean updateRecipe(Recipe r) {
-		Session sess = HibernateUtil.getSession();
-		Transaction tx;
-		tx = sess.beginTransaction();
-		try {			
-			sess.merge(r);
-			tx.commit();
+		Session sess = sf.getCurrentSession();
+		if(r!=null) {
+			sess.update(r);
 			return true;
-		} catch (HibernateException e) {
-			e.printStackTrace();
-			tx.rollback();
+		}else
 			return false;
-		} finally {
-			sess.close();
-		}
 	}
 
 	@Override
 	public boolean deleteRecipe(int id) {
-		Session sess = HibernateUtil.getSession();
-		Transaction tx;
-		tx = sess.beginTransaction();
-		try {
-			Recipe r = getById(id);
-			sess.delete(r);
-			tx.commit();
+		Session sess = sf.getCurrentSession();
+		if(id>0) {
+			sess.delete(id);
 			return true;
-		} catch (HibernateException e) {
-			e.printStackTrace();
-			tx.rollback();
+		}else
 			return false;
-		} finally {
-			sess.close();
-		}
 	}
 
 	@Override
 	public Recipe getById(int id) {
-		Session sess = HibernateUtil.getSession();
-		try {
-			return sess.get(Recipe.class, id);
-		} catch (HibernateException e) {
-			e.printStackTrace();
-			return null;
-		}
+		Session sess = sf.getCurrentSession();
+		return sess.get(Recipe.class, id);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Recipe> getByTitle(String title) {
-		//(Nikki) I'm not sure about this method? Open to changes
-		Session sess = HibernateUtil.getSession();
-		try {
-			return sess.createQuery("FROM recipes WHERE title = '"+title + "'").list();
-		}catch (HibernateException e) {
-			e.printStackTrace();
-			return new ArrayList<>();
-		}	
+		Session sess = sf.getCurrentSession();
+		Query<Recipe> q = sess.createQuery("FROM recipe WHERE title = :title");
+		q.setParameter("title", title);
+		return q.list();
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public List<Recipe> getAllByUserId(int id) {
-		Session sess = HibernateUtil.getSession();
-		try {
-			//not sure if will work
-			return sess.createQuery("FROM recipes WHERE owner.id = "+ id).list();
-		}catch (HibernateException e) {
-			e.printStackTrace();
-			return new ArrayList<>();
-		}	
+		Session sess = sf.getCurrentSession();
+		Query<Recipe> q = sess.createQuery("FROM recipe WHERE owner.id = :id");
+		q.setParameter("id", id);
+		return q.list();
 	}
 	
 	
